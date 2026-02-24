30e0bdd2ec2332033416eecf7d931ed547df7b8d
katom826 <katom826@gmail.com>
2026-02-25 02:40:31 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: "/Workout-timer",
+  assetPrefix: "/Workout-timer/",
 };
 
 export default nextConfig;
